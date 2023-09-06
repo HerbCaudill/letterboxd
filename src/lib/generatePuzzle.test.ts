2cd45821d1fe3ecc40989233a3c671822c3042d2
HerbCaudill <herb@devresults.com>
2023-09-06 15:58:12 +0200

    generatePuzzle

--- src/lib/generatePuzzle.test.ts
+++ src/lib/generatePuzzle.test.ts
@@ -1,12 +1,181 @@
-import { generatePuzzle } from './generatePuzzle'
+import { Layout, generateLayout, generatePuzzle, getAdjacencyMap } from './generatePuzzle'
+import { range } from './range'
 
-describe('generatePuzzle', () => {
-  it('should work', () => {
-    // expect(generatePuzzle('FREAKIEST THONGS')).toEqual([
-    //   new Set(['F', 'R', 'E']),
-    //   new Set(['A', 'K', 'I']),
-    //   new Set(['S', 'T', 'H']),
-    //   new Set(['N', 'G', 'O']),
-    // ])
+describe('getAdjacencyMap', () => {
+  const testCase = (solution: string, expected: Record<string, string>) => {
+    const adjacencyMap = {} as Record<string, Set<string>>
+    for (const letter in expected) {
+      adjacencyMap[letter] = new Set(expected[letter].split(''))
+    }
+    expect(getAdjacencyMap(solution)).toEqual(adjacencyMap)
+  }
+
+  it('FREAKIEST THONGS', () => {
+    testCase('FREAKIEST THONGS', {
+      F: 'R',
+      R: 'FE',
+      E: 'RAIS',
+      A: 'EK',
+      K: 'AI',
+      I: 'KE',
+      S: 'ETG',
+      T: 'SH',
+      H: 'TO',
+      O: 'HN',
+      N: 'OG',
+      G: 'NS',
+    })
+  })
+
+  it('ABNORMALITIES SENSITIZATION', () => {
+    testCase('ABNORMALITIES SENSITIZATION', {
+      A: 'BMLZT',
+      B: 'AN',
+      N: 'BOES',
+      O: 'NRI',
+      R: 'OM',
+      M: 'RA',
+      L: 'AI',
+      I: 'LTESZO',
+      T: 'IA',
+      E: 'ISN',
+      S: 'ENI',
+      Z: 'IA',
+    })
   })
 })
+
+describe('generateLayout', () => {
+  it('generates a layout when possible', () => {
+    const seed = 'test-1'
+    expect(layoutSummary(generateLayout('FREAKIEST THONGS', seed))).toEqual('AIO/EFT/GKR/HNS')
+    expect(layoutSummary(generateLayout('ABNORMALITIES SENSITIZATION', seed))).toEqual(
+      'AIN/BRZ/ELT/MOS'
+    )
+  })
+
+  it('throws an error when no layout is found', () => {
+    // this seed happens not to find a layout for this solution
+    const seed = 'test-2'
+    expect(() => generateLayout('ABNORMALITIES SENSITIZATION', seed)).toThrow()
+  })
+})
+
+describe('generatePuzzle', () => {
+  it('always returns a valid puzzle', () => {
+    const N = 100
+    const solutions = range(N).map(i => {
+      const { solution, layout } = generatePuzzle(`test-${i}`)
+      return `${solution} ${layoutSummary(layout)}`
+    })
+    expect(solutions).toMatchInlineSnapshot(`
+      [
+        "SOARING GEOMETRICAL ANS/CER/GMT/ILO",
+        "ENGINES SYMPTOMATOLOGY AOY/EGT/IPS/LMN",
+        "CLOTH HUMANITIES AIU/CHO/EMN/LST",
+        "DISCHARGED DECLARATIVE ASV/CIR/DGT/EHL",
+        "ENSUE EXCEPTIONAL AES/COT/ILX/NPU",
+        "PLAYHOUSE ELEGANT AHT/ENU/GPY/LOS",
+        "MISTAKENLY YARDS ANS/DTY/EIL/KMR",
+        "RELIEVES SYNAPTIC AEY/CTV/INP/LRS",
+        "OVENS SUBORDINATES AEI/BRS/DUV/NOT",
+        "INSOMNIA ADVENTURES ANO/DEM/ISU/RTV",
+        "HOSTAGE ELOQUENCE AHS/CGQ/EOT/LNU",
+        "PARLOUR RESTING ALS/EIO/GPR/NTU",
+        "FLASHED DURING AGI/DFR/ENS/HLU",
+        "HYDRODYNAMIC CLINICIANS AOS/CMN/DHL/IRY",
+        "FRIEND DETACHMENTS AFI/CNS/DMT/EHR",
+        "SCENT TRANSFORMATIVE ACI/EOT/FMN/RSV",
+        "PROTECTS SEMBLANCE ABC/EOP/LNS/MRT",
+        "PATENTS SORGHUM ANS/ERU/GMP/HOT",
+        "DISLOYAL LIGATURE ARS/DEO/GLU/ITY",
+        "RESOURCEFUL LECTURING CLS/EIU/FGO/NRT",
+        "SHARPLY YOUNGER AEN/GOS/HPY/LRU",
+        "YOUNGSTER RICHEST CGT/EUY/HIS/NOR",
+        "NUMBER REGULATORY AGO/BUY/ELT/MNR",
+        "INTENSELY YARDSTICK AEI/CRS/DNY/KLT",
+        "HOSTILITY YOUNGSTERS EGY/HIS/LTU/NOR",
+        "CONDITIONAL LOGARITHM ACH/DOR/GIM/LNT",
+        "AMENDING GLOBULES ABN/DEU/GMO/ILS",
+        "SEXTON NEIGHBOUR BEU/GNT/HIS/ORX",
+        "FIBROSIS SAMPLED AIR/BFM/DLO/EPS",
+        "SEPARATION NECKLACE AKN/COT/EIL/PRS",
+        "NONZERO OUTSTANDING ASZ/DGO/EIU/NRT",
+        "PROJECTIONS SHEIKH CIN/ERT/HJP/KOS",
+        "PROPER REVOLUTIONARY AET/ILP/NRV/OUY",
+        "ARGUING GRANDMOTHER AOU/DGH/EIT/MNR",
+        "SETUP PREDILECTION CDL/EOU/INR/PST",
+        "INTERFERON NEUROPATHY AFU/EHO/IRT/NPY",
+        "COURTIERS SOPHISTICATION AES/CTU/HOR/INP",
+        "ARCHAEOLOGY YOUTHS AOT/CEY/GSU/HLR",
+        "CEILING GUNBOATS ALS/BIU/CGT/ENO",
+        "RESPONSIBILITY YEAST AIO/BET/LNP/RSY",
+        "VERSE EXCLUDING CGR/DNV/EIL/SUX",
+        "ARITHMETIC CONVENIENCES AEH/CTV/IMO/NRS",
+        "PEAKS SOLIDARITY APS/DKT/ELR/IOY",
+        "PILOTS SQUARED AEQ/DRT/IOU/LPS",
+        "ASPARAGUS STUMBLED ABT/DLR/EPU/GMS",
+        "ARCHAEOLOGY YIELDS AOY/CDE/GHR/ILS",
+        "FETCH HEMOGLOBIN BGH/CFM/EIO/LNT",
+        "THORNS SPEAKING AGS/EIR/HKP/NOT",
+        "FALCON NERVOUSLY AEY/CRU/FLO/NSV",
+        "OVERVIEW WATCHMAN AIO/CMR/EHN/TVW",
+        "WETLANDS SUSPENSION AST/DEO/INW/LPU",
+        "NUCLEUS SUPERLATIVE AEN/CIP/LST/RUV",
+        "UPWARDS SIMULATOR AIP/DMT/LOS/RUW",
+        "FARES SUSPECTING ACP/EGI/FRU/NST",
+        "EUCALYPTUS SITUATIONAL AEP/CIN/LST/OUY",
+        "SANITY YOURSELF AEO/FTU/ILR/NSY",
+        "DANGEROUSLY YEOMAN ASY/DLO/ENU/GMR",
+        "CONTEMPORANEOUS STRAY AOT/CPY/ERU/MNS",
+        "CLOTHED DISPERSAL ADH/CRT/EOS/ILP",
+        "SPONSOR REPUBLICANS ABP/CEO/ISU/LNR",
+        "NONSENSE EPISTEMOLOGICAL AOS/CNT/EIL/GMP",
+        "CUTLER REMODELING CET/DGI/LMN/ORU",
+        "FACETS STUDYING ADI/CFT/ENY/GSU",
+        "BANTAM MICROSCOPE AEO/BNP/CMT/IRS",
+        "NEXUS STABILIZING AGI/BEU/LTZ/NSX",
+        "PROSPER RATIONALIZED AES/DOZ/INR/LPT",
+        "CONSENTING GRATIFIED AFN/CGS/DIR/EOT",
+        "MONUMENTS STRUCTURALISM AIT/CRS/EOU/LMN",
+        "COMFORTABLY YARNS ALS/BMY/CFR/NOT",
+        "SYNOVIAL LIGAMENTS AEV/GLO/INS/MTY",
+        "HOLDINGS SECTOR CDS/EIO/GRT/HLN",
+        "GUINEAS SPECIFICATION ANU/COP/EGI/FST",
+        "MONEYS SCRATCHED AMS/CEO/DNT/HRY",
+        "SORELY YARDSTICK AEO/CDT/ISY/KLR",
+        "JOURNEYS SOLIDITY DLY/EST/IOR/JNU",
+        "EARTHQUAKE ELIGIBLE ABQ/EHI/GKT/LRU",
+        "MODERATED DESTRUCTIVE ACO/DMT/EIU/RSV",
+        "RAMPANT TOUCHED AEU/CNP/DHO/MRT",
+        "INMATE EXPLODING ALN/DEP/GOX/IMT",
+        "SOCIETAL LAUGHED AGI/CEL/DOU/HST",
+        "SUNSHINE EXPROPRIATION ANP/EIU/HOX/RST",
+        "FADING GRUMBLED ALN/BGI/DMR/EFU",
+        "IDOLATRY YOURSELVES AEO/DLR/ISU/TVY",
+        "TOWARD DECLARING ADO/CGI/ELN/RTW",
+        "BONDED DISCOURAGE ASU/BEN/CGI/DOR",
+        "TWOFOLD DIRECTING CDN/EGI/FLW/ORT",
+        "AMBIVALENT TURBO AEO/BLN/IRT/MUV",
+        "LIMIT TRANSDUCTION AOU/CIS/DLT/MNR",
+        "MAGNIFY YOUTHS AIO/FGH/MUY/NST",
+        "HERMENEUTIC CLAMP AEI/CRT/HNP/LMU",
+        "INVESTED DEPLOYMENT DNS/ELY/IMT/OPV",
+        "RESTAURANT TWITCHING ACW/EGT/HNU/IRS",
+        "REFRACTORY YAWNING AEI/CNO/FTY/GRW",
+        "DIABETES SCULPTURED ADR/BST/CIL/EPU",
+        "INTERVIEWER REPULSION ESU/IPR/LOT/NVW",
+        "NAVIGATING GEOMETRICAL AMR/CNT/EIL/GOV",
+        "GENETIC CARBONYL AEL/BIY/COR/GNT",
+        "TOPICAL LEISURELY APT/CES/ILU/ORY",
+        "CATEGORIZE EDIFICES AEO/CDF/GIS/RTZ",
+        "PRECIPITATES SWAYING ACR/EGI/NPS/TWY",
+      ]
+    `)
+  })
+})
+
+const layoutSummary = (layout: Layout) => {
+  const sides = Array.from(layout).map(side => Array.from(side).sort().join(''))
+  return sides.sort().join('/')
+}
--- src/lib/generatePuzzle.ts
+++ src/lib/generatePuzzle.ts
@@ -1,13 +1,81 @@
+import { makeRandom } from '@herbcaudill/random'
 import { distinctLetters, generateRandomSolution } from './words'
 
-export const generatePuzzle = (solution = generateRandomSolution()): Set<string>[] => {
-  const letters = distinctLetters(solution)
-
-  // this is not correct
-  return [
-    new Set(letters.slice(0, 3)),
-    new Set(letters.slice(3, 6)),
-    new Set(letters.slice(6, 9)),
-    new Set(letters.slice(9, 12)),
-  ]
+export const generatePuzzle = (seed = Math.random().toString()) => {
+  const random = makeRandom(seed)
+  while (true) {
+    const solution = generateRandomSolution(seed)
+    try {
+      const layout = generateLayout(solution, seed)
+      return { solution, layout }
+    } catch (e) {
+      // if we can't find a valid layout, we'll just try again with a new seed
+      console.log(`Unable to find a valid layout for ${solution}`)
+      seed = random.alpha(10)
+    }
+  }
 }
+
+export const generateLayout = (solution: string, seed = Math.random().toString()): Layout => {
+  const random = makeRandom(seed)
+  const layout = [] as Layout
+  const adjacencyMap = getAdjacencyMap(solution)
+
+  for (const letter of distinctLetters(solution)) {
+    // for each letter, the options are:
+    // (1) find an existing side that has room and doesn't contain any adjacent letters
+    const options = layout.filter(
+      side =>
+        // side isn't full
+        side.size < 3 &&
+        // side doesn't contain any adjacent letters
+        !Array.from(side).some(l => adjacencyMap[letter].has(l))
+    )
+
+    // OR (2) create a new side, if we don't have 4 yet
+    if (layout.length < 4) options.push(new Set() as Side)
+
+    if (options.length === 0) {
+      // if there are no options, there are two possibilities:
+      // - maybe if we'd made different choices earlier, we could find a valid layout
+      // - maybe there's not a valid layout for this solution
+      // not sure how to handle that yet
+      // for now we'll just throw an error and the caller will have to choose a different solution
+      throw new Error(`Unable to find a valid layout`)
+    } else {
+      // otherwise we pick an option randomly
+      const side = random.pick(options)
+      if (side.size === 0) layout.push(side)
+      side.add(letter)
+    }
+  }
+
+  return layout
+}
+
+export const getAdjacencyMap = (solution: string) => {
+  const solutionSequence = solution
+    .split(' ')
+    .map((word, i) => {
+      if (i === 0) return word
+      else return word.substring(1)
+    })
+    .join('')
+
+  return distinctLetters(solution).reduce<AdjacencyMap>((result, letter) => {
+    const adjacentLetters = result[letter] || new Set<string>()
+    let i = -1
+    while (true) {
+      i = solutionSequence.indexOf(letter, i + 1)
+      if (i === -1) break
+      if (i > 0) adjacentLetters.add(solutionSequence[i - 1])
+      if (i < solutionSequence.length - 1) adjacentLetters.add(solutionSequence[i + 1])
+    }
+    result[letter] = adjacentLetters
+    return result
+  }, {})
+}
+
+export type Side = Set<string> // each side has three letters; the order doesn't matter
+export type Layout = Side[] // each layout has 4 sides; the order doesn't matter but we use an array for convenience
+export type AdjacencyMap = { [letter: string]: Set<string> }
