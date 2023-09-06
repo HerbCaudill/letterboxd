6e0973ab735254c79542cde306dba2899e184894
HerbCaudill <herb@devresults.com>
2023-09-06 17:34:01 +0200

    move some stuff around

--- src/lib/generatePuzzle.ts
+++ src/lib/generatePuzzle.ts
@@ -1,5 +1,5 @@
 import { makeRandom } from '@herbcaudill/random'
-import { distinctLetters, generateRandomSolution } from './words'
+import { commonWords, distinctLetterCount, distinctLetters } from './words'
 
 export const generatePuzzle = (seed = Math.random().toString()) => {
   const random = makeRandom(seed)
@@ -10,7 +10,6 @@ export const generatePuzzle = (seed = Math.random().toString()) => {
       return { solution, layout }
     } catch (e) {
       // if we can't find a valid layout, we'll just try again with a new seed
-      console.log(`Unable to find a valid layout for ${solution}`)
       seed = random.alpha(10)
     }
   }
@@ -35,45 +34,80 @@ export const generateLayout = (solution: string, seed = Math.random().toString()
     // OR (2) create a new side, if we don't have 4 yet
     if (layout.length < 4) options.push(new Set() as Side)
 
-    if (options.length === 0) {
-      // if there are no options, there are two possibilities:
-      // - maybe if we'd made different choices earlier, we could find a valid layout
-      // - maybe there's not a valid layout for this solution
-      // not sure how to handle that yet
-      // for now we'll just throw an error and the caller will have to choose a different solution
-      throw new Error(`Unable to find a valid layout`)
-    } else {
-      // otherwise we pick an option randomly
-      const side = random.pick(options)
-      if (side.size === 0) layout.push(side)
-      side.add(letter)
-    }
+    // if there are no options, there are two possibilities:
+    // - maybe if we'd made different choices earlier, we could find a valid layout
+    // - maybe there's not a valid layout for this solution
+    // solutions are cheap so we'll just throw an error and get a new one
+    if (options.length === 0) throw new Error(`Unable to find a valid layout`)
+
+    // otherwise we pick one of the options randomly
+    const side = random.pick(options)
+    if (side.size === 0) layout.push(side)
+    side.add(letter)
   }
 
   return layout
 }
 
+/**
+ * Returns a map of each letter to the letters that are adjacent to it in the given solution.
+ * For example, for the solution "FREAKIEST THONGS", the map would look like this:
+ *   F: [R]
+ *   R: [F,E]
+ *   E: [R,A,I,S]
+ *   A: [E,K]
+ *   K: [A,I]
+ *   I: [K,E]
+ *   S: [E,T,G]
+ *   T: [S,H]
+ *   H: [T,O]
+ *   O: [H,N]
+ *   N: [O,G]
+ *   G: [N,S]
+ */
 export const getAdjacencyMap = (solution: string) => {
-  const solutionSequence = solution
+  // First we need to convert the solution into a single sequence of letters, removing the space
+  // between the two words and eliminating the repeated letter in the middle, so for example
+  // FREAKIEST THONGS -> FREAKIESTHONGS
+  const sequence = solution
     .split(' ')
     .map((word, i) => {
       if (i === 0) return word
       else return word.substring(1)
     })
-    .join('')
+    .flatMap(word => word.split(''))
 
-  return distinctLetters(solution).reduce<AdjacencyMap>((result, letter) => {
+  // now we go through that sequence and build up the adjacency map
+  return sequence.reduce((result, letter) => {
     const adjacentLetters = result[letter] || new Set<string>()
     let i = -1
+    // find each instance of this letter in the sequence
     while (true) {
-      i = solutionSequence.indexOf(letter, i + 1)
-      if (i === -1) break
-      if (i > 0) adjacentLetters.add(solutionSequence[i - 1])
-      if (i < solutionSequence.length - 1) adjacentLetters.add(solutionSequence[i + 1])
+      i = sequence.indexOf(letter, i + 1)
+      if (i === -1) break // no more instances of this letter
+      if (i > 0) adjacentLetters.add(sequence[i - 1]) // if this isn't the first letter, the previous letter
+      if (i < sequence.length - 1) adjacentLetters.add(sequence[i + 1]) // if this isn't the last letter, the next letter
     }
-    result[letter] = adjacentLetters
-    return result
-  }, {})
+    return {
+      ...result,
+      [letter]: adjacentLetters,
+    }
+  }, {} as AdjacencyMap)
+}
+
+export function generateRandomSolution(seed = Math.random().toString()) {
+  const rand = makeRandom(seed)
+  const w1 = rand.pick(commonWords)
+  const finalLetter = w1.substring(w1.length - 1, w1.length)
+  const pairs = commonWords
+    .filter(w2 => w2.startsWith(finalLetter))
+    .map(w2 => w1 + ' ' + w2)
+    .filter(pair => distinctLetterCount(pair) === 12)
+  if (pairs.length === 0) {
+    // unable to find a solution with the starting word; try again with a new seed
+    return generateRandomSolution(rand.alpha(20).toString())
+  }
+  return rand.pick(pairs)
 }
 
 export type Side = Set<string> // each side has three letters; the order doesn't matter
--- src/lib/words.ts
+++ src/lib/words.ts
@@ -25,18 +25,3 @@ const allWords = commonWords.concat(uncommonWords)
 
 const allWordsSet = new Set(allWords)
 export const isValid = (word: string) => allWordsSet.has(word)
-
-export function generateRandomSolution(seed = Math.random().toString()) {
-  const rand = makeRandom(seed)
-  const w1 = rand.pick(commonWords)
-  const finalLetter = w1.substring(w1.length - 1, w1.length)
-  const pairs = commonWords
-    .filter(w2 => w2.startsWith(finalLetter))
-    .map(w2 => w1 + ' ' + w2)
-    .filter(pair => distinctLetterCount(pair) === 12)
-  if (pairs.length === 0) {
-    // unable to find a solution with the starting word; try again with a new seed
-    return generateRandomSolution(rand.alpha(20).toString())
-  }
-  return rand.pick(pairs)
-}
