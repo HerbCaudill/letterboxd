6de2e2ef5fcd3fc47edfd76141dfab3950801117
HerbCaudill <herb@devresults.com>
2023-09-07 18:47:18 +0200

    display layout in the correct order

--- src/components/Cursor.tsx
+++
@@ -1,6 +0,0 @@
-export const Cursor = ({ scale }: { scale: number }) => (
-  <div
-    className="inline-block bg-black relative animate-blink"
-    style={{ top: scale * 0.2, width: 3, height: scale * 1.2 }}
-  ></div>
-)
--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,10 +1,17 @@
 import { useReducer } from 'react'
 import { reducer } from 'reducer'
 import { Layout, State } from 'types'
-import { Cursor } from './Cursor'
 import { useKeyboard } from 'hooks/useKeyboard'
 import { isAlpha } from 'lib/isAlpha'
-import { layoutToString } from 'lib/generateLayout'
+
+// constants
+const size = 525
+const squareSize = size * 0.5
+const origin = (size - squareSize) / 2
+const bubbleSize = squareSize * 0.03
+const stroke = squareSize * 0.012
+const fontSize = squareSize * 0.11
+const scale = 25
 
 export const Game = ({ layout }: Props) => {
   const initialState: State = {
@@ -14,17 +21,13 @@ export const Game = ({ layout }: Props) => {
     error: null,
   }
 
-  console.log(layoutToString(layout))
   const [state, dispatch] = useReducer(reducer, initialState)
 
-  const letters = layout.flatMap(side => Array.from(side))
-
   const nextLetterCanBe = (letter: string) => {
-    console.log('nextLetterCanBe', { letter, state })
     letter = letter.toUpperCase()
 
     // letter must be in layout
-    if (!letters.includes(letter)) return false
+    if (!layout.some(letters => letters.has(letter))) return false
 
     // if current word is empty, any letter in the layout is valid
     if (state.currentWord.length === 0) {
@@ -35,8 +38,6 @@ export const Game = ({ layout }: Props) => {
     // otherwise, letter must be not be on the same side as the previous letter
     const prevLetter = state.currentWord.slice(-1)
     const prevLetterSide = layout.find(side => side.has(prevLetter))
-
-    console.log({ prevLetter, prevLetterSide })
     return !prevLetterSide?.has(letter)
   }
 
@@ -46,59 +47,18 @@ export const Game = ({ layout }: Props) => {
     else if (key === 'Enter') dispatch({ type: 'ENTER' })
   })
 
-  const size = 525
-  const squareSize = size * 0.5
-  const origin = (size - squareSize) / 2
-  const bubbleSize = squareSize * 0.03
-  const stroke = squareSize * 0.012
-  const fontSize = squareSize * 0.11
-
-  const bubbles = (side: 'top' | 'bottom' | 'left' | 'right') => {
-    const xOffset = side === 'left' ? -fontSize * 0.6 : side === 'right' ? fontSize * 0.6 : 0
-    const yOffset =
-      side === 'top' ? -fontSize * 0.6 : side === 'bottom' ? fontSize * 1.4 : fontSize * 0.3
-
-    // alignment of text
-    const textAnchor =
-      side === 'left'
-        ? 'end' // left side: right align
-        : side === 'right'
-        ? 'start' // right side: left align
-        : 'middle' // top & bottom sides: center align
-
-    const sidePosition = origin + squareSize * (side === 'top' || side === 'left' ? 0 : 1)
-    return [0, 1, 2].map(i => {
-      const bubblePosition = origin + (squareSize * (1 + 2 * i)) / 6
-      return {
-        xOffset,
-        yOffset,
-        textAnchor,
-        side,
-        ...(side === 'top' || side === 'bottom'
-          ? { x: bubblePosition, y: sidePosition }
-          : { x: sidePosition, y: bubblePosition }),
-      }
-    })
-  }
-
-  const Bubble = ({ x, y }: { x: number; y: number }) => (
-    <circle cx={x} cy={y} r={bubbleSize} fill="white" stroke="black" strokeWidth={stroke} />
-  )
-
-  const scale = 25
-
   return (
     <>
       <div className={`select-none`}>
         <div
           className="border-b-[3px] h-12 border-black flex justify-center content-center items-center font-bold"
-          style={{ height: scale * 2 }}
+          style={{ height: 50 }}
         >
           <div style={{ fontSize: scale, lineHeight: 1 }}>{state.currentWord}</div>
           {/* cursor */}
           <div
             className="bg-black animate-blink ml-1"
-            style={{ top: scale * 0.2, width: 3, height: scale * 1.2 }}
+            style={{ top: 3, width: 3, height: 28 }}
           ></div>
         </div>
 
@@ -118,26 +78,57 @@ export const Game = ({ layout }: Props) => {
             stroke="black"
             strokeWidth={stroke}
           />
-          // white circles with black borders
-          {[...bubbles('top'), ...bubbles('right'), ...bubbles('bottom'), ...bubbles('left')].map(
-            ({ x, y, xOffset, yOffset, textAnchor }, i) => {
-              // position of text relative to bubble
+          {layout.flatMap((letters, i) => {
+            const side = ['top', 'right', 'bottom', 'left'][i]
+
+            // offset of text relative to bubble
+            const xOffset =
+              side === 'left' ? -fontSize * 0.6 : side === 'right' ? fontSize * 0.6 : 0
+            const yOffset =
+              side === 'top' ? -fontSize * 0.6 : side === 'bottom' ? fontSize * 1.4 : fontSize * 0.3
+
+            // alignment of text
+            const textAnchor =
+              side === 'left'
+                ? 'end' // left side: right align
+                : side === 'right'
+                ? 'start' // right side: left align
+                : 'middle' // top & bottom sides: center align
+
+            const sidePosition = origin + squareSize * (side === 'top' || side === 'left' ? 0 : 1)
+            return Array.from(letters).map((letter, i) => {
+              const bubblePosition = origin + (squareSize * (1 + 2 * i)) / 6
+
+              const position =
+                side === 'top' || side === 'bottom'
+                  ? { x: bubblePosition, y: sidePosition }
+                  : { x: sidePosition, y: bubblePosition }
+
               return (
-                <g key={i}>
-                  <Bubble x={x} y={y} />
+                <g>
+                  {/* white circle with black borders */}
+                  <circle
+                    cx={position.x}
+                    cy={position.y}
+                    r={bubbleSize}
+                    fill="white"
+                    stroke="black"
+                    strokeWidth={stroke}
+                  />
+                  {/* letter */}
                   <text
-                    x={x + xOffset}
-                    y={y + yOffset}
+                    x={position.x + xOffset}
+                    y={position.y + yOffset}
                     style={{ fontSize }}
                     textAnchor={textAnchor}
                     className={`font-sans font-semibold fill-white`}
                   >
-                    {letters[i]}
+                    {letter}
                   </text>
                 </g>
               )
-            }
-          )}
+            })
+          })}
         </svg>
       </div>
     </>
