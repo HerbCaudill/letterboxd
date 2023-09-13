dc8da3ff50e7b2f11fd5d3b995807053c50d3c75
HerbCaudill <herb@devresults.com>
2023-09-13 14:44:56 +0200

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,43 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import type { VitePWAOptions } from 'vite-plugin-pwa'
+import { VitePWA as vitePWA } from 'vite-plugin-pwa'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-// https://vitejs.dev/config https://vitest.dev/config
+const pwaOptions: Partial<VitePWAOptions> = {
+  mode: 'production',
+  includeAssets: ['favicon.ico'],
+  srcDir: 'src',
+  registerType: 'autoUpdate',
+  strategies: 'injectManifest',
+  injectManifest: {
+    globPatterns: ['**/*.{js,css,html,ico,json,svg}'],
+  },
+  manifest: {
+    name: 'Letterboxd',
+    short_name: 'Letterboxd',
+    description: 'A recreation of the NYT game Letterboxd',
+    theme_color: '#000000',
+    background_color: '#FAA6A4',
+    display: 'standalone',
+    icons: [
+      {
+        src: 'logo.svg',
+        sizes: '48x48 72x72 96x96 128x128 256x256 512x512',
+        type: 'image/svg+xml',
+        purpose: 'any',
+      },
+    ],
+  },
+  devOptions: {
+    enabled: false,
+    type: 'module',
+    navigateFallback: 'index.html',
+  },
+}
+
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), vitePWA(pwaOptions)],
   test: {
     globals: true,
     environment: 'happy-dom',
