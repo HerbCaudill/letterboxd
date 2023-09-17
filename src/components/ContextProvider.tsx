05623c17da2d935f6615457cacff801c95a2662e
HerbCaudill <herb@devresults.com>
2023-09-17 19:53:34 +0200

    ContextProvider.tsx:

--- src/components/ContextProvider.tsx
+++ src/components/ContextProvider.tsx
@@ -10,11 +10,16 @@ import { Action, initializer, reducer } from '../reducer'
 // We can also specify a date in the query string to get the puzzle for that date.
 let date = String(queryString('date') || getUtcDate())
 
-let level = String(queryString('level')) as Level
-if (!Object.keys(levels).includes(level)) level = 'hard'
+const getLevel = (): Level => {
+  const level = String(queryString('level'))
+  return Object.keys(levels).includes(level) ? (level as Level) : 'hard'
+}
+
+const level = getLevel()
 
 const initialState =
-  storage.get(date, level) || initializer(generatePuzzleWithLevel(level, date).layout)
+  storage.get(date, level) || // get from local storage if available
+  initializer(generatePuzzleWithLevel(level, date).layout) // otherwise generate puzzle for this date
 
 export const Context = createContext<ProviderPayload>(null!)
 
